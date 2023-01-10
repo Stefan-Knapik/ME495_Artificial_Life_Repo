c81750c663e156f76b82dd1e03022742fe6aef56
stefo99 <stefanknapik2025@u.northwestern.edu>
2023-01-09 20:43:09 -0600

    B17

--- simulation.py
+++ simulation.py
@@ -1,5 +1,15 @@
 import pybullet as p
+import time
+
+num_steps = 1000
+time_step_size = 1/60
 
 physicsClient = p.connect(p.GUI)
+
+for i in range(num_steps):
+    p.stepSimulation()
+
+    print(i)
+    time.sleep(time_step_size)
 
 p.disconnect()
