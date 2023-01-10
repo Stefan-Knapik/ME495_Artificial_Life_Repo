5ecb29acca8339d3e914f24918066b67161a8f22
stefo99 <stefanknapik2025@u.northwestern.edu>
2023-01-09 22:13:03 -0600

    E27

--- generate.py
+++ generate.py
@@ -16,6 +16,8 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[x, y, z] , size=[length, width, height])
+    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [length/2,0,height])
+    pyrosim.Send_Cube(name="Leg", pos=[x+length, y, z+height] , size=[length, width, height])
     pyrosim.End()
 
 Create_World()
