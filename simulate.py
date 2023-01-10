c52f7d3078e06f2531461d7820a1f8ad2105647d
stefo99 <stefanknapik2025@u.northwestern.edu>
2023-01-09 21:16:27 -0600

    D9

--- simulate.py
+++ simulate.py
@@ -1,11 +1,15 @@
 import pybullet as p
+import pybullet_data
 import time
 
 num_steps = 1000
 time_step_size = 1/200
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
 
 for i in range(num_steps):
