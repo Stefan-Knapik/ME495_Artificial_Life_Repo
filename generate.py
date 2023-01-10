142629116ba2fd6a371ceee957bb70d1d820def2
stefo99 <stefanknapik2025@u.northwestern.edu>
2023-01-09 21:28:32 -0600

    D27

--- generate.py
+++ generate.py
@@ -8,7 +8,8 @@ x = 0
 y = 0
 z = height/2
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
+pyrosim.Send_Cube(name="Box2", pos=[x, y, z] , size=[length, width, height])
 
 pyrosim.End()
