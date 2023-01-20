710a064049e582d882428f70952efd7bc3ff22e6
stefo99 <stefanknapik2025@u.northwestern.edu>
2023-01-20 03:19:21 -0600

    J79

--- generate.py
+++ generate.py
@@ -38,8 +38,14 @@ def Generate_Brain():
     pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
     pyrosim.Send_Motor_Neuron(name = 3 , jointName = "Torso_BackLeg")
     pyrosim.Send_Motor_Neuron(name = 4 , jointName = "Torso_FrontLeg")
-    pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 3 , weight = 1.0)
-    pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1.0)
+    magn = -.3
+    pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 3 , weight = magn)
+    pyrosim.Send_Synapse(sourceNeuronName = 2 , targetNeuronName = 3 , weight = 0.0)
+    pyrosim.Send_Synapse(sourceNeuronName = 1 , targetNeuronName = 4 , weight = 0.0)
+    pyrosim.Send_Synapse(sourceNeuronName = 2 , targetNeuronName = 4 , weight = magn)
+    pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 3 , weight = magn * 1.8)
+    pyrosim.Send_Synapse(sourceNeuronName = 0 , targetNeuronName = 4 , weight = magn * 1.8)
+    
     pyrosim.End()
 
 Create_World()
