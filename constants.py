a5ceb846f67bcfece285965b055b8c3f62b7ba9c
stefo99 <stefanknapik2025@u.northwestern.edu>
2023-02-07 02:13:59 -0600

    update

--- constants.py
+++ constants.py
@@ -1,29 +1,29 @@
 from math import pi
 
-numLinks = 12
-numSensorNeurons = numLinks
-numMotorNeurons = numLinks - 1
+# Visualisation
+progress_bar = 1
+printFitness = 0
 
+# Body 
+numLinks = 12 # number of links in the body
 radius = 1 # radius of circumscribed circle
 thickness = 0.1 # thickness of the hoop, radial
 width = 1 # width of the hoop, axial
 
-progress_bar = 1
-printFitness = 0
-
+# Optimization 
 numberOfGenerations = 200
 populationSize = 20
-randomPercentage = 0.1
-bredPercentage = 0.4
+# Reproduction
+randomPercentage = 0.1 # proportion of random children
+bredPercentage = 0.4 # proportion of bred children
 
+# Simulation settings
 num_steps = 2000
 sleep_time = 1/240
-
 gravity = -9.8
+
+# Control 
 maxForce = 100
 motorJointRange = 0.2
-
-# maxForce = 20
-# amplitude = pi/6
-# frequency = 13
-# phaseOffset = pi/12
+numSensorNeurons = numLinks
+numMotorNeurons = numLinks - 1
