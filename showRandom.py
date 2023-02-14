cc8ff4e2f73f227304a355d004681b0fc9970275
stefo99 <stefanknapik2025@u.northwestern.edu>
2023-02-13 23:48:54 -0600

    update

--- showRandom.py
+++ showRandom.py
@@ -1,10 +1,3 @@
-# import os
-# from parallelHillClimber import PARALLEL_HILL_CLIMBER
-
-# phc = PARALLEL_HILL_CLIMBER()
-
-# phc.Evolve()
-# phc.Show_Best()
 import os
 from solution import SOLUTION
 
