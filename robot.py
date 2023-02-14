80c468e8313e13c6a1a86aa7e3688640f9f5a983
Stefan-Knapik <stefanknapik2025@u.northwestern.edu>
2023-02-13 21:27:04 -0600

    update

--- robot.py
+++ robot.py
@@ -37,6 +37,12 @@ class ROBOT:
             if self.nn.Is_Motor_Neuron(neuronName):
                 jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
                 desiredAngle = self.nn.Get_Value_Of(neuronName) * c.motorJointRange
+                
+                print(self.motors)
+                print(jointName)
+                print(self.motors[jointName])
+                exit()
+                
                 self.motors[jointName].Set_Value(desiredAngle, self.robotId)
             
     def Think(self):
