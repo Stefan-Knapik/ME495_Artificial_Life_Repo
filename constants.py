efd2a664b580bd20bf40a9ea7ac6229292af10a5
stefo99 <stefanknapik2025@u.northwestern.edu>
2023-03-14 06:04:44 -0500

    update

--- constants.py
+++ constants.py
@@ -1,20 +1,20 @@
 from math import pi
 
-trials = 1
-num_to_save = 4
+trials = 2
+num_to_save = 5
 
 progress_bar = True
 printFitness = False
 printCollision = False
 
-numberOfGenerations = 100
-populationSize = 16
+numberOfGenerations = 5
+populationSize = 6
 
 num_steps = 1600
 sleep_time = 1/240
 
 gravity = -9.8
-maxForce = 50
+maxForce = 60
 motorJointRange = 1
 
 # AFPO
