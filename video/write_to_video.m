9382a62389eb9f598890181388e65f3ae52a2e21
stefo99 <stefanknapik2025@u.northwestern.edu>
2023-03-14 23:26:46 -0500

    update

--- video/write_to_video.m
+++ video/write_to_video.m
@@ -2,16 +2,17 @@ clc; clear; format compact;
 
 N = 1000;
 algo = "BOTH"
-seed = "4";
+seed = "gif";
 
-name = "video_" + algo + "_" + string(N) + "_" + seed + ".gif"
+name = "video_" + algo + "_" + string(N) + "_" + seed + ".avi"
 
 video = VideoWriter(name); %create the video object
 video.FrameRate = ceil(N/10);
+video.FrameRate = 20;
 
 open(video); %open the file for writing
 
-for i = 1:N %where N is the number of images
+for i = [1,5:5:N] %where N is the number of images
 
     str = sprintf('Generation%04d.png',i);
 
